e44230cab064fd46d4515143b04a563034e1d72f
sica-lab1 <richmond.liew.97@gmail.com>
2020-02-25 10:30:04 -0500

    Flowgraphs/batNoGui.grc

--- WebInterface/batmanNoGui.py
+++ WebInterface/batmanNoGui.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Batmannogui
-# Generated: Thu Mar 10 19:02:06 2016
+# Generated: Mon Feb 24 16:46:25 2020
 ##################################################
 import threading
 
@@ -22,7 +22,7 @@ from optparse import OptionParser
 
 class batmanNoGui(gr.top_block):
 
-    def __init__(self, ampl=0.7, args='', arq_timeout=.1*0 + 0.04, dest_addr=-1, iface='tun0', mtu=128, ogradio_addr=0, port="12345", rate=1e6, rx_antenna="TX/RX", rx_gain=65-20, rx_lo_offset=0, samps_per_sym=4, tx_gain=45, tx_lo_offset=0, max_arq_attempts=5 * 2, tx_freq=915e6, rx_freq=915e6):
+    def __init__(self, ampl=0.7, args='', arq_timeout=.1*0 + 0.04, dest_addr=-1, iface='tun0', max_arq_attempts=5 * 2, mtu=128, ogradio_addr=0, port='12345', rate=1e6, rx_antenna='TX/RX', rx_freq=915e6, rx_gain=65-20, rx_lo_offset=0, samps_per_sym=4, tx_freq=915e6, tx_gain=45, tx_lo_offset=0):
         gr.top_block.__init__(self, "Batmannogui")
 
         self._lock = threading.RLock()
@@ -35,19 +35,19 @@ class batmanNoGui(gr.top_block):
         self.arq_timeout = arq_timeout
         self.dest_addr = dest_addr
         self.iface = iface
+        self.max_arq_attempts = max_arq_attempts
         self.mtu = mtu
         self.ogradio_addr = ogradio_addr
         self.port = port
         self.rate = rate
         self.rx_antenna = rx_antenna
+        self.rx_freq = rx_freq
         self.rx_gain = rx_gain
         self.rx_lo_offset = rx_lo_offset
         self.samps_per_sym = samps_per_sym
+        self.tx_freq = tx_freq
         self.tx_gain = tx_gain
         self.tx_lo_offset = tx_lo_offset
-        self.max_arq_attempts = max_arq_attempts
-        self.tx_freq = tx_freq
-        self.rx_freq = rx_freq
 
         ##################################################
         # Variables
@@ -59,25 +59,23 @@ class batmanNoGui(gr.top_block):
         ##################################################
         self.gmsk_radio_0 = gmsk_radio(
             rate=samp_rate,
-            args=args,
-            tx_gain=tx_gain,
-            tx_freq=tx_freq,
-            rx_ant=rx_antenna,
-            rx_freq=rx_freq,
-            rx_gain=rx_gain,
-            ampl=ampl,
-            rx_lo_offset=rx_lo_offset,
-            tx_lo_offset=tx_lo_offset,
             samps_per_sym=samps_per_sym,
+            ampl=ampl,
             access_code_threshold=0 + 12 + 4*0,
         )
+        self.blocks_udp_source_0 = blocks.udp_source(gr.sizeof_gr_complex*1, '127.0.0.1', 12345, 1472, True)
         self.blocks_tuntap_pdu_0 = blocks.tuntap_pdu(iface, mtu*0 + 1532, False)
+        self.blocks_tcp_server_sink_0 = blocks.tcp_server_sink(gr.sizeof_gr_complex*1, '192.168.1.3', 12344, True)
+
+
 
         ##################################################
         # Connections
         ##################################################
-        self.msg_connect((self.blocks_tuntap_pdu_0, 'pdus'), (self.gmsk_radio_0, 'msg_in'))    
-        self.msg_connect((self.gmsk_radio_0, 'msg_out'), (self.blocks_tuntap_pdu_0, 'pdus'))    
+        self.msg_connect((self.blocks_tuntap_pdu_0, 'pdus'), (self.gmsk_radio_0, 'msg_in'))
+        self.msg_connect((self.gmsk_radio_0, 'msg_out'), (self.blocks_tuntap_pdu_0, 'pdus'))
+        self.connect((self.blocks_udp_source_0, 0), (self.gmsk_radio_0, 0))
+        self.connect((self.gmsk_radio_0, 0), (self.blocks_tcp_server_sink_0, 0))
 
     def get_ampl(self):
         return self.ampl
@@ -93,7 +91,6 @@ class batmanNoGui(gr.top_block):
     def set_args(self, args):
         with self._lock:
             self.args = args
-            self.gmsk_radio_0.set_args(self.args)
 
     def get_arq_timeout(self):
         return self.arq_timeout
@@ -115,6 +112,13 @@ class batmanNoGui(gr.top_block):
     def set_iface(self, iface):
         with self._lock:
             self.iface = iface
+
+    def get_max_arq_attempts(self):
+        return self.max_arq_attempts
+
+    def set_max_arq_attempts(self, max_arq_attempts):
+        with self._lock:
+            self.max_arq_attempts = max_arq_attempts
 
     def get_mtu(self):
         return self.mtu
@@ -151,7 +155,13 @@ class batmanNoGui(gr.top_block):
     def set_rx_antenna(self, rx_antenna):
         with self._lock:
             self.rx_antenna = rx_antenna
-            self.gmsk_radio_0.set_rx_ant(self.rx_antenna)
+
+    def get_rx_freq(self):
+        return self.rx_freq
+
+    def set_rx_freq(self, rx_freq):
+        with self._lock:
+            self.rx_freq = rx_freq
 
     def get_rx_gain(self):
         return self.rx_gain
@@ -159,7 +169,6 @@ class batmanNoGui(gr.top_block):
     def set_rx_gain(self, rx_gain):
         with self._lock:
             self.rx_gain = rx_gain
-            self.gmsk_radio_0.set_rx_gain(self.rx_gain)
 
     def get_rx_lo_offset(self):
         return self.rx_lo_offset
@@ -167,7 +176,6 @@ class batmanNoGui(gr.top_block):
     def set_rx_lo_offset(self, rx_lo_offset):
         with self._lock:
             self.rx_lo_offset = rx_lo_offset
-            self.gmsk_radio_0.set_rx_lo_offset(self.rx_lo_offset)
 
     def get_samps_per_sym(self):
         return self.samps_per_sym
@@ -177,13 +185,19 @@ class batmanNoGui(gr.top_block):
             self.samps_per_sym = samps_per_sym
             self.gmsk_radio_0.set_samps_per_sym(self.samps_per_sym)
 
+    def get_tx_freq(self):
+        return self.tx_freq
+
+    def set_tx_freq(self, tx_freq):
+        with self._lock:
+            self.tx_freq = tx_freq
+
     def get_tx_gain(self):
         return self.tx_gain
 
     def set_tx_gain(self, tx_gain):
         with self._lock:
             self.tx_gain = tx_gain
-            self.gmsk_radio_0.set_tx_gain(self.tx_gain)
 
     def get_tx_lo_offset(self):
         return self.tx_lo_offset
@@ -191,30 +205,6 @@ class batmanNoGui(gr.top_block):
     def set_tx_lo_offset(self, tx_lo_offset):
         with self._lock:
             self.tx_lo_offset = tx_lo_offset
-            self.gmsk_radio_0.set_tx_lo_offset(self.tx_lo_offset)
-
-    def get_max_arq_attempts(self):
-        return self.max_arq_attempts
-
-    def set_max_arq_attempts(self, max_arq_attempts):
-        with self._lock:
-            self.max_arq_attempts = max_arq_attempts
-
-    def get_tx_freq(self):
-        return self.tx_freq
-
-    def set_tx_freq(self, tx_freq):
-        with self._lock:
-            self.tx_freq = tx_freq
-            self.gmsk_radio_0.set_tx_freq(self.tx_freq)
-
-    def get_rx_freq(self):
-        return self.rx_freq
-
-    def set_rx_freq(self, rx_freq):
-        with self._lock:
-            self.rx_freq = rx_freq
-            self.gmsk_radio_0.set_rx_freq(self.rx_freq)
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -226,7 +216,7 @@ class batmanNoGui(gr.top_block):
 
 
 def argument_parser():
-    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    parser = OptionParser(usage="%prog: [options]", option_class=eng_option)
     parser.add_option(
         "", "--ampl", dest="ampl", type="eng_float", default=eng_notation.num_to_str(0.7),
         help="Set TX BB amp [default=%default]")
@@ -243,20 +233,26 @@ def argument_parser():
         "", "--iface", dest="iface", type="string", default='tun0',
         help="Set Interface name [default=%default]")
     parser.add_option(
+        "", "--max-arq-attempts", dest="max_arq_attempts", type="intx", default=5 * 2,
+        help="Set Max ARQ attempts [default=%default]")
+    parser.add_option(
         "", "--mtu", dest="mtu", type="intx", default=128,
         help="Set MTU [default=%default]")
     parser.add_option(
         "-l", "--ogradio-addr", dest="ogradio_addr", type="intx", default=0,
         help="Set Local address [default=%default]")
     parser.add_option(
-        "", "--port", dest="port", type="string", default="12345",
+        "", "--port", dest="port", type="string", default='12345',
         help="Set TCP port [default=%default]")
     parser.add_option(
         "-r", "--rate", dest="rate", type="eng_float", default=eng_notation.num_to_str(1e6),
         help="Set Sample rate [default=%default]")
     parser.add_option(
-        "-A", "--rx-antenna", dest="rx_antenna", type="string", default="TX/RX",
+        "-A", "--rx-antenna", dest="rx_antenna", type="string", default='TX/RX',
         help="Set RX antenna [default=%default]")
+    parser.add_option(
+        "", "--rx-freq", dest="rx_freq", type="eng_float", default=eng_notation.num_to_str(915e6),
+        help="Set RX freq [default=%default]")
     parser.add_option(
         "", "--rx-gain", dest="rx_gain", type="eng_float", default=eng_notation.num_to_str(65-20),
         help="Set RX gain [default=%default]")
@@ -267,20 +263,14 @@ def argument_parser():
         "", "--samps-per-sym", dest="samps_per_sym", type="intx", default=4,
         help="Set Samples/symbol [default=%default]")
     parser.add_option(
+        "", "--tx-freq", dest="tx_freq", type="eng_float", default=eng_notation.num_to_str(915e6),
+        help="Set TX freq [default=%default]")
+    parser.add_option(
         "", "--tx-gain", dest="tx_gain", type="eng_float", default=eng_notation.num_to_str(45),
         help="Set TX gain [default=%default]")
     parser.add_option(
         "", "--tx-lo-offset", dest="tx_lo_offset", type="eng_float", default=eng_notation.num_to_str(0),
         help="Set TX LO offset [default=%default]")
-    parser.add_option(
-        "", "--max-arq-attempts", dest="max_arq_attempts", type="intx", default=5 * 2,
-        help="Set Max ARQ attempts [default=%default]")
-    parser.add_option(
-        "", "--tx-freq", dest="tx_freq", type="eng_float", default=eng_notation.num_to_str(915e6),
-        help="Set TX freq [default=%default]")
-    parser.add_option(
-        "", "--rx-freq", dest="rx_freq", type="eng_float", default=eng_notation.num_to_str(915e6),
-        help="Set RX freq [default=%default]")
     return parser
 
 
@@ -288,7 +278,7 @@ def main(top_block_cls=batmanNoGui, options=None):
     if options is None:
         options, _ = argument_parser().parse_args()
 
-    tb = top_block_cls(ampl=options.ampl, args=options.args, arq_timeout=options.arq_timeout, dest_addr=options.dest_addr, iface=options.iface, mtu=options.mtu, ogradio_addr=options.ogradio_addr, port=options.port, rate=options.rate, rx_antenna=options.rx_antenna, rx_gain=options.rx_gain, rx_lo_offset=options.rx_lo_offset, samps_per_sym=options.samps_per_sym, tx_gain=options.tx_gain, tx_lo_offset=options.tx_lo_offset, max_arq_attempts=options.max_arq_attempts, tx_freq=options.tx_freq, rx_freq=options.rx_freq)
+    tb = top_block_cls(ampl=options.ampl, args=options.args, arq_timeout=options.arq_timeout, dest_addr=options.dest_addr, iface=options.iface, max_arq_attempts=options.max_arq_attempts, mtu=options.mtu, ogradio_addr=options.ogradio_addr, port=options.port, rate=options.rate, rx_antenna=options.rx_antenna, rx_freq=options.rx_freq, rx_gain=options.rx_gain, rx_lo_offset=options.rx_lo_offset, samps_per_sym=options.samps_per_sym, tx_freq=options.tx_freq, tx_gain=options.tx_gain, tx_lo_offset=options.tx_lo_offset)
     tb.start()
     tb.wait()
 
